63ce191fcb6271f67c8ef29d1dd2dc7eecc09154
Ajver <zawlockidominik@gmail.com>
2018-08-08 14:20:49 +0200

    Generating mines, changes...

--- js/game.js
+++ js/game.js
@@ -28,11 +28,11 @@ function Field(x, y) {
         if(this.neighbors > 0) {
           this.htmlEl.innerHTML = this.neighbors;
           switch(this.neighbors) {
-            case 1: this.htmlEl.style.color = '#038428'; break;
-            case 2: this.htmlEl.style.color = '#007599'; break;
+            case 1: this.htmlEl.style.color = '#025419'; break;
+            case 2: this.htmlEl.style.color = '#004c7a'; break;
             case 3: this.htmlEl.style.color = '#e29700'; break;
-            case 4: this.htmlEl.style.color = '#d63900'; break;
-            default: this.htmlEl.style.color = '#a30606'; break;
+            case 4: this.htmlEl.style.color = '#a30606'; break;
+            default: this.htmlEl.style.color = '#560707'; break;
           }
         }
       }
@@ -76,6 +76,20 @@ function Field(x, y) {
       e.preventDefault();
     }, false);
   }
+  
+  this.countNeighbors = function(fields, col, row) {
+    for(var xx=this.x-1; xx<this.x+2; xx++) {
+      for(var yy=this.y-1; yy<this.y+2; yy++) {
+        if(xx >= 0 && xx < col &&
+           yy >= 0 && yy < row &&
+           (xx != this.x || yy != this.y)) {
+          if(fields[xx][yy].isMine) {
+            this.neighbors++;
+          }
+        }
+      }
+    }
+  }
 }
 
 function Map(level) {
@@ -86,12 +100,12 @@ function Map(level) {
   this.htmlEl = null;
   
   this.open = function (x, y) {
-    if (!this.fields[x][y].isOpen && !this.fields[x][y].isPointed) {
+    if(!this.fields[x][y].isOpen && !this.fields[x][y].isPointed) {
       this.fields[x][y].open();
 
-      if (this.fields[x][y].isMine) { // Game over
+      if(this.fields[x][y].isMine) { // Game over
 
-      } else if (this.fields[x][y].neighbors == 0) {
+      }else if(this.fields[x][y].neighbors == 0) {
         function Point(x, y) {
           this.x = x;
           this.y = y;
@@ -101,29 +115,48 @@ function Map(level) {
 
         points.push(new Point(x, y));
 
-        while (points.length > 0) {
+        while(points.length > 0) {
           var newPoints = [];
 
-          for (var i = 0; i < points.length; i++) {
+          for(var i = 0; i < points.length; i++) {
             for(var yy=points[i].y - 1; yy<points[i].y + 2; yy++) {
             for(var xx=points[i].x - 1; xx<points[i].x + 2; xx++) {
-              if(xx >= 0 && xx < this.level.col) {
-                if(yy >= 0 && yy < this.level.row) {
-                  if(!this.fields[xx][yy].isOpen && !this.fields[xx][yy].isPointed) {
-                    if(this.fields[xx][yy].neighbors == 0) {
-                      newPoints.push(new Point(xx, yy));
-                    }
-
-                    this.fields[xx][yy].open();
+              if(xx >= 0 && xx < this.level.col &&
+                 yy >= 0 && yy < this.level.row) {
+                if(!this.fields[xx][yy].isOpen && !this.fields[xx][yy].isPointed) {
+                  if(this.fields[xx][yy].neighbors == 0) {
+                    newPoints.push(new Point(xx, yy));
                   }
+
+                  this.fields[xx][yy].open();
                 }
               }
+              
             }}
-            
             points = newPoints;
           }
         }
       }
+    }
+    
+    // Checking is won 
+    var won = true;
+    
+    for(var xx=0; yy<this.level.col; xx++) {
+    for(var yy=0; yy<this.level.row; yy++) {
+      if(!this.fields[xx][yy].isMine) {
+        if(!this.fields[xx][yy].isOpen) {
+          won = false;
+          // Breaking the loop
+          xx = this.level.col;
+          yy = this.level.row;
+          break;
+        }
+      }
+    }}
+    
+    if(won) {
+      
     }
   }
   
@@ -138,6 +171,38 @@ function Map(level) {
     this.fields[x][y].updateHTMLElement();
   }
   
+  this.createMap = function() {
+    var margin = this.level.col < 32 ? 2 : 1;
+    var fieldW = (this.w/this.level.col) - (margin*2);
+    var fieldH = (this.w/this.level.row) - (margin*2);
+    
+    for(var xx=0; xx<this.level.col; xx++) {
+      this.fields[xx] = [];
+      for(var yy=0; yy<this.level.row; yy++) {
+        this.fields[xx][yy] = new Field(xx, yy, fieldW);       
+        this.fields[xx][yy].createHTMLElement(fieldW, fieldH, margin);
+      }
+    }
+    
+    // Generating mines 
+    for(var i=0; i<this.level.mines; i++) {
+      var mx, my;
+      do {
+        mx = Math.floor(Math.random() * this.level.col);
+        my = Math.floor(Math.random() * this.level.row);
+      }while(this.fields[mx][my].isMine);
+      
+      this.fields[mx][my].isMine = true;
+    }
+    
+    // Counting neighbors
+    for(var xx=0; xx<this.level.col; xx++) {
+      for(var yy=0; yy<this.level.row; yy++) {
+        this.fields[xx][yy].countNeighbors(this.fields, this.level.col, this.level.row);
+      }
+    }
+  }
+  
   this.createHTMLElement = function() {
     this.htmlEl = document.createElement("div");
     this.htmlEl.classList.add("map");
@@ -146,17 +211,11 @@ function Map(level) {
       e.preventDefault();
     }, false);
     
-    var margin = this.level.col < 32 ? 2 : 1;
-    var fieldW = (this.w/this.level.col) - (margin*2);
-    var fieldH = (this.w/this.level.row) - (margin*2);
+    this.createMap();
     
     for(var xx=0; xx<this.level.col; xx++) {
-      this.fields[xx] = [];
       for(var yy=0; yy<this.level.row; yy++) {
-        var f = new Field(xx, yy, fieldW);       
-        f.createHTMLElement(fieldW, fieldH, margin);
-        this.fields[xx][yy] = f;
-        this.htmlEl.appendChild(f.htmlEl);
+        this.htmlEl.appendChild(this.fields[xx][yy].htmlEl);
       }
     }
     
